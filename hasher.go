74e1e1ea1577958fc17c2c43d2b6baba8f2243a5
Andy Arthur <andy@dolthub.com>
2022-12-20 10:24:48 -0800

    add short README, doc comments, .gitignore

--- hasher.go
+++ hasher.go
@@ -16,17 +16,23 @@ package maphash
 
 import "unsafe"
 
+// Hasher hashes values of type K.
+// Uses runtime AES-based hashing.
 type Hasher[K comparable] struct {
 	hash hashfn
 	seed uintptr
 }
 
+// NewHasher creates a new Hasher[K] with a random seed.
 func NewHasher[K comparable]() Hasher[K] {
 	h, s := getRuntimeHasher[K]()
 	return Hasher[K]{hash: h, seed: s}
 }
 
+// Hash hashes |key|.
 func (h Hasher[K]) Hash(key K) uint64 {
+	// promise to the compiler that pointer
+	// |p| does not escape the stack.
 	p := noescape(unsafe.Pointer(&key))
 	return uint64(h.hash(p, h.seed))
 }
